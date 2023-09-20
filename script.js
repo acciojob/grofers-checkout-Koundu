692081c79f1bc3d835334bfaa75255fd2e37246d
gk1706 <121346044+Koundu@users.noreply.github.com>
2023-09-20 14:25:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,8 +14,9 @@ const getSum = () => {
 	let tr     = document.createElement('tr');
 	let c1 = document.createElement("td");
 	let c2 = document.createElement("td");
-	c1.innerText = "Total Price"
+	c1.innerText = "Total Price";
 	c2.innerText = sum;
+	c2.setAttribute("id","ans")
 	tr.appendChild(c1);
 	tr.appendChild(c2);
 	table.appendChild(tr);
